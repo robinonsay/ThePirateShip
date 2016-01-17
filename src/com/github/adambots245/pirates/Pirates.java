d13a9b7f885be059e70d2b198bf252eb90bf0386
raomax <raomax@gmail.com>
2016-01-16 20:19:03 -0500

    Added to Ship, Created Pirates, Added to main
    I made the Ship class static, Added pirate phrases to Pirates, and
    executed them in Main

---
+++ src/com/github/adambots245/pirates/Pirates.java
@@ -0,0 +1,12 @@
+package com.github.adambots245.pirates;
+
+public class Pirates{
+
+  private static String[] piratePhrases = {"arg", "Ahoy!", "LandHo!"};
+
+  public static String getRandomPiratePhrase(){
+    int maxIndex = piratePhrases.length;
+
+    return piratePhrases[(int) (Math.random() * maxIndex)];
+  }
+}
