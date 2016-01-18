9b424b1112f9c4e356925ae021bbfb82ab97db06
robinonsay <raomax@gmail.com>
2016-01-18 14:04:35 -0500

    Added two new pirate phrases
    Added Matey, and walk the plank

--- src/com/github/adambots245/pirates/Pirates.java
+++ src/com/github/adambots245/pirates/Pirates.java
@@ -2,7 +2,7 @@ package com.github.adambots245.pirates;
 
 public class Pirates{
 
-  private static String[] piratePhrases = {"arg", "Ahoy!", "LandHo!"};
+  private static String[] piratePhrases = {"arg", "Ahoy!", "LandHo!", "Matey", "walk the plank"};
 
   public static String getRandomPiratePhrase(){
     int maxIndex = piratePhrases.length;
