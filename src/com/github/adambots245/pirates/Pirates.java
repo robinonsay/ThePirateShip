e9643b9738e6c800d5248ec0b6e3a22ccad2063b
Gabe-Kubiak <gabekubiak@gmail.com>
2016-01-18 14:24:09 -0500

    Changed Pirate Phrases
    Corrected capitalization, punctuation, and spelling errors in pirate
    phrases.

--- src/com/github/adambots245/pirates/Pirates.java
+++ src/com/github/adambots245/pirates/Pirates.java
@@ -2,7 +2,7 @@ package com.github.adambots245.pirates;
 
 public class Pirates{
 
-  private static String[] piratePhrases = {"arg", "Ahoy!", "LandHo!", "Matey", "walk the plank"};
+  private static String[] piratePhrases = {"Arghh...!", "Ahoy!", "Land Ho!", "Matey", "Walk the plank!"};
 
   public static String getRandomPiratePhrase(){
     int maxIndex = piratePhrases.length;
